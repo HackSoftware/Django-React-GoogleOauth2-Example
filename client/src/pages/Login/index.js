06b075dc52b25199c0752e2919f8e602bec2ab68
martin056 <martinangelov056@gmail.com>
2021-03-21 13:54:38 +0200

    Add react-google-button

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { GoogleLogin } from 'react-google-login';
 
 const Login = () => {
-  return <h1>Login</h1>;
+  const onGoogleLoginSuccess = useCallback((response) => {
+    console.log(response);
+  }, []);
+
+  return (
+    <div>
+      <h1>Login</h1>
+
+      <GoogleLogin
+        clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+        buttonText='Login with Google'
+        onSuccess={onGoogleLoginSuccess}
+        onFailure={console.log}
+        cookiePolicy={'single_host_origin'}
+      />
+    </div>
+  );
 };
 
 export default Login;
